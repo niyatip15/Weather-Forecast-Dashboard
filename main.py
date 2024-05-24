4289d5f85f8fe8438806d77632ac6287438e5f98
niyatip15 <niyupathak1503@gmail.com>
2024-05-24 17:51:04 +0530

    Fixed Bugs

--- main.py
+++ main.py
@@ -10,18 +10,21 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} {'day' if days == 1 else 'days'} in {place}")
 
 if place:
-    filtered_data = get_weather_data(place,days)
+    try:
+        filtered_data = get_weather_data(place,days)
 
-    if option == "Temperature":
-        temperatures = [weather_dict["main"]["temp"] for weather_dict in filtered_data]
-        dates = [weather_dict["dt_txt"] for weather_dict in filtered_data]
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Dates", "y": "Temperature(C)"})
-        st.plotly_chart(figure)
-    elif option == "Sky":
-        images = {"Clear": "images/clear.png","Clouds":"images/cloud.png",
-                  "Rain":"images/rain.png","Snow":"images/snow.png"}
-        filtered_data = [weather_dict["weather"][0]["main"] for weather_dict in filtered_data]
-        image_paths = [images[condition] for condition in filtered_data]
-        st.image(image_paths,width=115)
-    else:
-        option = None
+        if option == "Temperature":
+            temperatures = [weather_dict["main"]["temp"] / 10 for weather_dict in filtered_data]
+            dates = [weather_dict["dt_txt"] for weather_dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Dates", "y": "Temperature(C)"})
+            st.plotly_chart(figure)
+        elif option == "Sky":
+            images = {"Clear": "images/clear.png","Clouds":"images/cloud.png",
+                    "Rain":"images/rain.png","Snow":"images/snow.png"}
+            filtered_data = [weather_dict["weather"][0]["main"] for weather_dict in filtered_data]
+            image_paths = [images[condition] for condition in filtered_data]
+            st.image(image_paths,width=115)
+        else:
+            option = None
+    except KeyError:
+        st.write("This place does not exist.")
